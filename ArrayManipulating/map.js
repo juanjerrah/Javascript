990c0bb113ad43e84df2c7b7dd12769f53ed0b4c
Juan Jerrah <juanjerrah15@gmail.com>
2021-08-29 17:34:20 -0300

    Criando primeira manipulação de array [Map]

---
+++ ArrayManipulating/map.js
@@ -0,0 +1,14 @@
+let values = [5, 3, 7, 10,8, 2, 9, 11, 20];
+
+let doubleValues = values.map((value) => {
+    return value * 2;
+});
+
+let sumDoubleValues = doubleValues.map((value) => {
+    return 
+    
+});
+
+console.log(values);
+console.log(doubleValues);
+console.log(sumDoubleValues);
