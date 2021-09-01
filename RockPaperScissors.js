f3ec82917b03d45d7a642d7d9ed42684d01ef580
Juan Jerrah <juanjerrah15@gmail.com>
2021-09-01 10:24:11 -0300

    Removi os comentários desnecessários

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -72,36 +72,3 @@ let rockPaperScissors = () => {
 
 console.log(rockPaperScissors());
 
-/** 
-let escolhas = {
-    1:'Pedra',
-    2:'Papel',
-    3: 'Tesoura'
-}
-console.log('Escolha o número \n1-Pedra \n2-Papel \n3-Tesoura')
-let playerChoose = input("Digite o número que corresponde a sua escolha: ")
-let machineChoose = '';
-let random = Math.floor( Math.random() * 3 );
-
-switch (random) {
-    case 0:
-        machineChoose = 'Pedra'
-        break;
-    case 1:
-        machineChoose = 'Papel'
-        break;
-    case 3:
-        machineChoose = 'Tesoura'
-        break;
-    default:
-        break;
-}
-
-if((playerChoose === 1 && random === 0) || 
-    (playerChoose === 2 && random === 1) ||
-    (playerChoose === 3 && random === 2) ) {
-    
-        console.log(`Você = ${escolha.playerChoose}`)
-        console.log(`Maquina = ${machineChoose}`)
-}
-*/
